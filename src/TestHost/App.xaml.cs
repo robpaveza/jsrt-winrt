8bbe1373f5862f227d3128e2ca61505902db8e53
Chris Anderson (WINDOWS) <chrisan@microsoft.com>
2015-12-18 13:28:41 -0800

    Remove tests (now that they are in a proper test)... move TestHost to be a demo of hosting the engine

--- src/TestHost/App.xaml.cs
+++ src/TestHost/App.xaml.cs
@@ -43,7 +43,7 @@ namespace TestHost
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                // this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 
